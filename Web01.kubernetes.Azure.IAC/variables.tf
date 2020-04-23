2775a3d5f019cac73554a253a4561a04dcf39c30
BUENAFLOR, Gerardo C <63630691+GBuenaflor@users.noreply.github.com>
2020-04-23 13:48:46 +0800

    Update variables.tf

--- Web01.kubernetes.Azure.IAC/variables.tf
+++ Web01.kubernetes.Azure.IAC/variables.tf
@@ -11,25 +11,33 @@
 #---------------------------------------------------- 
 
 
-variable subscription_id {
-    default = "2b5ee6f8-e317-4bc2-bced-a565aca7d8c1" 
- }
+variable subscription_id {}
+# variable subscription_id {
+#   default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" 
+#}
 
-variable client_id       {
-    default = "22146dd3-9ba9-4f63-8062-4f681a19be6f"
- }
+variable client_id {}
+# variable client_id       {
+#    default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+# }
+variable client_secret {}
 
-variable client_secret   {
-    default = "cb351e77-c2d7-4534-a357-56d15f7173f3"
- }
+# variable client_secret   {
+#    default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+# }
 
-variable tenant_id       {
-    default = "0a5c2b53-e3e6-45c2-aa3c-6827c8a81934"
- }
+variable tenant_id {}
+# variable tenant_id       {
+#    default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+# }
 
-variable ssh_public_key {
-    default = "azure_rsa.pub"
-}
+variable ssh_public_key {}
+# azure_rsa.pub is uploaded in AzureDevOps Secure filethen set up the azure devops variable
+# or upload the azure_rsa.pub together with the terrafom files and use the set up below
+# variable ssh_public_key {
+#    default = "azure_rsa.pub"
+#}
+ 
 
 #----------------------------------------------------
 # Azure Storage Account
@@ -39,10 +47,10 @@ variable storage_account_name {
     default = "dev01straccnt01"
 }
  
-# Running Terraform Manualy, add this value:
-variable access_key {
-    default = "1qxGagOl73iVmc/KBZJvKS1aHHG/MIDS6BHRd3OXi5PFav6fNBPv3h0xhEr1zvHsaLkMzHw/UcOxMf09yNnSsQ==" 
-}
+variable access_key {}
+#variable access_key {
+#   default = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
+#}
   
 variable key {
     default = "dev01.k8s.tfstate"
